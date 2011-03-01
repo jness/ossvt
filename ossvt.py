6445ba7e54fa8310769140099d1543803bcab39d
Jeffrey Ness <jness@flip-edesign.com>
2011-03-01 14:08:17 -0600

    combine check and ossvt scripts

--- ossvt.py
+++ ossvt.py
@@ -1,32 +1,41 @@
 #!/usr/bin/env python
+
 from lib.upstream import *
 from lib.ius import *
 from lib.launchpad import *
+import argparse
+
+class colors:
+    pink = '\033[95m'
+    red = '\033[91m'
+    green = '\033[92m'
+    gold = '\033[93m'
+    blue = '\033[94m'
+    end = '\033[0m'
+
+
+# Build my Parser with help for user input
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', help='Software Version to Compare', required=False)
+args = parser.parse_args()
 
 # Lets compare IUS version with latest upstream
-packages = packages()
+if args.name:
+    pkg = package(args.name)
+else:
+    pkg = packages()
 
-for p in packages:
+# Print out our Packages and Info
+print '%-30s %-15s %-15s %s' % ('name', 'ius ver', 'upstream ver', 'status')
+print '='*75
+
+for p in pkg:
     upstream_ver = latest(p)
     ius_ver = ius_version(p['name'])
 
     # Do the actual version comparisons
     compare = compare_to_ius(ius_ver, upstream_ver)
     if compare:
-
-        # See if we checked LP Yet
-        try:
-            if titles:
-                pass
-        except:
-            # If we haven't checked LP do it now
-            titles = bug_titles()
-
-        if compare_titles(titles, p['name'], compare):
-            # Our version is outdated and a LP bug does not exist
-            print 'Bug Already Created'
-        else:
-            print ' Creating Launchpad Bug for', p['name']
-            #create_bug(package, compare, p['url'])
+        print '%-30s %-15s %-15s %s' % (p['name'], ius_ver, upstream_ver, colors.red + 'outdated' + colors.end)
     else:
-        print p['name'], 'is up to date, we have', ius_ver, 'upstream has', upstream_ver
+        print '%-30s %-15s %-15s %s' % (p['name'], ius_ver, upstream_ver, colors.green + 'updated' + colors.end)
