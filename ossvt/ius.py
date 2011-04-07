2964e8ccd716d46fffc4587c9f69527dd89beebe
Jeffrey Ness <jness@flip-edesign.com>
2011-04-07 11:14:07 -0500

    seems i forgot to push some updates

--- ossvt/ius.py
+++ ossvt/ius.py
@@ -7,8 +7,12 @@ def ius_stable(name):
     request = urllib2.urlopen('http://dl.iuscommunity.org/pub/ius/stable/Redhat/5/SRPMS/')
     content = request.read()
     match = compile(name + '-([0-9.]*)-.*.src.rpm').findall(content)
-    ius_ver = sorted(match, reverse=True)[0]
-    return ius_ver
+    try:
+        ius_ver = sorted(match, reverse=True)[0]
+    except IndexError:
+        return None
+    else
+        return ius_ver
 
 def ius_testing(name):
     '''Using regex we pull the latest version number from the IUS RHEL5 repo '''
