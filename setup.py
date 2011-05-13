c5a861628b5cab7d67825adf8bfb15a5c97192d7
Jeffrey Ness <jness@flip-edesign.com>
2011-05-12 19:02:02 -0500

    adding nose test

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(name='ossvt',
           'configobj',
           'launchpadlib',
       ],
+      test_suite='nose.collector',
       entry_points= {
         'console_scripts': ['ossvt = ossvt.main:main']
         },
